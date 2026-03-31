f3ab78b6dfd50e6f31b13c31773fac7e24af5177
tuowang <2461233967@qq.com>
2026-03-31 20:49:35 +0800

    fix: fix bug

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 # pre_train
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 torchrun --nproc_per_node=6 trainer/trainer_pretrain2.py --use_wandb \
-  --batch_size 120 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 trainer/train_pretrain.py --use_wandb \
+  --batch_size 130 \
   --epochs 2 \
   --num_hidden_layers 8 \
   --hidden_size 768 \
   --data_path /root/autodl-tmp/pretrain_t2t.jsonl \
+  --log_interval 50 \
   --use_compile 1 && /usr/bin/shutdown  
 # CUDA_VISIBLE_DEVICES=0 python trainer/trainer_pretrain2.py --use_wandb \
 #   --batch_size 16 \
--- trainer/train_pretrain.py
+++ trainer/train_pretrain.py
@@ -51,7 +51,7 @@ def train_epoch(epoch, loader, iters, start_step=0, wandb=None):
         if step % args.log_interval == 0 or step == iters:
             spend_time = time.time() - start_time
             current_loss = loss.item() * args.accumulation_steps
-            current_aux_loss = res.aux_loss.item() if res.aux_loss is not None else 0.0
+            current_aux_loss =  0.0
             current_logits_loss = current_loss - current_aux_loss
             current_lr = optimizer.param_groups[-1]['lr']
             eta_min = spend_time / max(step - start_step, 1) * (iters - step) // 60
@@ -90,7 +90,7 @@ if __name__ == "__main__":
     parser.add_argument("--device", type=str, default="cuda:0" if torch.cuda.is_available() else "cpu", help="训练设备")
     parser.add_argument("--dtype", type=str, default="bfloat16", help="混合精度类型")
     parser.add_argument("--num_workers", type=int, default=8, help="数据加载线程数")
-    parser.add_argument("--accumulation_steps", type=int, default=8, help="梯度累积步数")
+    parser.add_argument("--accumulation_steps", type=int, default=5, help="梯度累积步数")
     parser.add_argument("--grad_clip", type=float, default=1.0, help="梯度裁剪阈值")
     parser.add_argument("--log_interval", type=int, default=100, help="日志打印间隔")
     parser.add_argument("--save_interval", type=int, default=1000, help="模型保存间隔")
